60fdf9266bd94dc37c62ff44342b1aea52162a54
Ben W. Brumfield <benwbrum@gmail.com>
2019-10-08 11:07:11 -0500

    notes on converting from audacity to WA

---
+++ examples/her_kind/annotations/audacity/script/convert.rb
@@ -0,0 +1,115 @@
+raw = '{"@context": "http://iiif.io/api/presentation/2/context.json", "@id": "48520b9f-39d2-4109-b11f-18140fa29179", "@type": "oa:Annotation", "motivation": "sc:painting", "annotatedBy": {"name": "ben"}, "resource": {"@type": "dctypes:Text", "format": "text/html", "chars": "<p>Another user annotation</p>", "language": "en"}, "on": {"full": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/canvas/32862204.5243.emory.edu$0", "@type": "oa:SpecificResource", "within": {"@id": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/manifest", "@type": "sc:Manifest"}, "selector": {"@type": "oa:FragmentSelector", "value": "xywh=463,1577,969,413", "item": {"@type": "oa:Choice", "value": "<svg xmlns=\'http://www.w3.org/2000/svg\'><path xmlns=\\"http://www.w3.org/2000/svg\\" d=\\"M462.66116,1577.33884h484.47107v0h484.47107v206.47521v206.47521h-484.47107h-484.47107v-206.47521z\\" data-paper-data=\\"{&quot;strokeWidth&quot;:1,&quot;rotation&quot;:0,&quot;deleteIcon&quot;:null,&quot;rotationIcon&quot;:null,&quot;group&quot;:null,&quot;newlyCreatedStrokeFactor&quot;:5,&quot;newlyCreated&quot;:true,&quot;editable&quot;:true,&quot;annotation&quot;:null}\\" id=\\"rectangle_6f4342ce-8f70-4190-a166-384eb614921e\\" fill-opacity=\\"0\\" fill=\\"#00bfff\\" fill-rule=\\"nonzero\\" stroke=\\"#00bfff\\" stroke-width=\\"5\\" stroke-linecap=\\"butt\\" stroke-linejoin=\\"miter\\" stroke-miterlimit=\\"10\\" stroke-dasharray=\\"\\" stroke-dashoffset=\\"0\\" font-family=\\"none\\" font-weight=\\"none\\" font-size=\\"none\\" text-anchor=\\"none\\" style=\\"mix-blend-mode: normal\\"/></svg>", "default": {"@type": "oa:FragmentSelector", "value": "xywh=463,1577,969,413"}}}}}, {"@context": "http://iiif.io/api/presentation/2/context.json", "@id": "dd687ad4-a845-4f66-8bcc-ad5a12d58c4e", "@type": "oa:Annotation", "motivation": "sc:painting", "annotatedBy": {"name": "ben"}, "resource": {"@type": "dctypes:Text", "format": "text/html", "chars": "<p>testing saving a user annotation</p>", "language": "en"}, "on": {"full": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/canvas/32862204.5243.emory.edu$0", "@type": "oa:SpecificResource", "within": {"@id": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/manifest", "@type": "sc:Manifest"}, "selector": {"@type": "oa:FragmentSelector", "value": "xywh=970,917,1396,340", "item": {"@type": "oa:Choice", "value": "<svg xmlns=\'http://www.w3.org/2000/svg\'><path xmlns=\\"http://www.w3.org/2000/svg\\" d=\\"M970.30579,916.59504h698.00413v0h698.00413v170.21488v170.21488h-698.00413h-698.00413v-170.21488z\\" data-paper-data=\\"{&quot;strokeWidth&quot;:1,&quot;rotation&quot;:0,&quot;deleteIcon&quot;:null,&quot;rotationIcon&quot;:null,&quot;group&quot;:null,&quot;newlyCreatedStrokeFactor&quot;:5,&quot;newlyCreated&quot;:true,&quot;editable&quot;:true,&quot;annotation&quot;:null}\\" id=\\"rectangle_8500a0f5-ae4c-4ad2-af9b-1f090825cb15\\" fill-opacity=\\"0\\" fill=\\"#00bfff\\" fill-rule=\\"nonzero\\" stroke=\\"#00bfff\\" stroke-width=\\"5\\" stroke-linecap=\\"butt\\" stroke-linejoin=\\"miter\\" stroke-miterlimit=\\"10\\" stroke-dasharray=\\"\\" stroke-dashoffset=\\"0\\" font-family=\\"none\\" font-weight=\\"none\\" font-size=\\"none\\" text-anchor=\\"none\\" style=\\"mix-blend-mode: normal\\"/></svg>", "default": {"@type": "oa:FragmentSelector", "value": "xywh=970,917,1396,340"}}}}}'
+raw
+JSON.parse(raw)
+print raw
+raw = '{"@context": "http://iiif.io/api/presentation/2/context.json", "@id": "48520b9f-39d2-4109-b11f-18140fa29179", "@type": "oa:Annotation", "motivation": "sc:painting", "annotatedBy": {"name": "ben"}, "resource": {"@type": "dctypes:Text", "format": "text/html", "chars": "<p>Another user annotation</p>", "language": "en"}, "on": {"full": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/canvas/32862204.5243.emory.edu$0", "@type": "oa:SpecificResource", "within": {"@id": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/manifest", "@type": "sc:Manifest"}, "selector": {"@type": "oa:FragmentSelector", "value": "xywh=463,1577,969,413", "item": {"@type": "oa:Choice", "value": "<svg xmlns=\'http://www.w3.org/2000/svg\'><path xmlns=\\"http://www.w3.org/2000/svg\\" d=\\"M462.66116,1577.33884h484.47107v0h484.47107v206.47521v206.47521h-484.47107h-484.47107v-206.47521z\\" data-paper-data=\\"{&quot;strokeWidth&quot;:1,&quot;rotation&quot;:0,&quot;deleteIcon&quot;:null,&quot;rotationIcon&quot;:null,&quot;group&quot;:null,&quot;newlyCreatedStrokeFactor&quot;:5,&quot;newlyCreated&quot;:true,&quot;editable&quot;:true,&quot;annotation&quot;:null}\\" id=\\"rectangle_6f4342ce-8f70-4190-a166-384eb614921e\\" fill-opacity=\\"0\\" fill=\\"#00bfff\\" fill-rule=\\"nonzero\\" stroke=\\"#00bfff\\" stroke-width=\\"5\\" stroke-linecap=\\"butt\\" stroke-linejoin=\\"miter\\" stroke-miterlimit=\\"10\\" stroke-dasharray=\\"\\" stroke-dashoffset=\\"0\\" font-family=\\"none\\" font-weight=\\"none\\" font-size=\\"none\\" text-anchor=\\"none\\" style=\\"mix-blend-mode: normal\\"/></svg>", 
+"default": {"@type": "oa:FragmentSelector", "value": "xywh=463,1577,969,413"}}}}}'
+JSON.parse(raw)
+raw = '[{"@context": "http://iiif.io/api/presentation/2/context.json", "@id": "48520b9f-39d2-4109-b11f-18140fa29179", "@type": "oa:Annotation", "motivation": "sc:painting", "annotatedBy": {"name": "ben"}, "resource": {"@type": "dctypes:Text", "format": "text/html", "chars": "<p>Another user annotation</p>", "language": "en"}, "on": {"full": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/canvas/32862204.5243.emory.edu$0", "@type": "oa:SpecificResource", "within": {"@id": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/manifest", "@type": "sc:Manifest"}, "selector": {"@type": "oa:FragmentSelector", "value": "xywh=463,1577,969,413", "item": {"@type": "oa:Choice", "value": "<svg xmlns=\'http://www.w3.org/2000/svg\'><path xmlns=\\"http://www.w3.org/2000/svg\\" d=\\"M462.66116,1577.33884h484.47107v0h484.47107v206.47521v206.47521h-484.47107h-484.47107v-206.47521z\\" data-paper-data=\\"{&quot;strokeWidth&quot;:1,&quot;rotation&quot;:0,&quot;deleteIcon&quot;:null,&quot;rotationIcon&quot;:null,&quot;group&quot;:null,&quot;newlyCreatedStrokeFactor&quot;:5,&quot;newlyCreated&quot;:true,&quot;editable&quot;:true,&quot;annotation&quot;:null}\\" id=\\"rectangle_6f4342ce-8f70-4190-a166-384eb614921e\\" fill-opacity=\\"0\\" fill=\\"#00bfff\\" fill-rule=\\"nonzero\\" stroke=\\"#00bfff\\" stroke-width=\\"5\\" stroke-linecap=\\"butt\\" stroke-linejoin=\\"miter\\" stroke-miterlimit=\\"10\\" stroke-dasharray=\\"\\" stroke-dashoffset=\\"0\\" font-family=\\"none\\" font-weight=\\"none\\" font-size=\\"none\\" text-anchor=\\"none\\" style=\\"mix-blend-mode: normal\\"/></svg>", 
+"default": {"@type": "oa:FragmentSelector", "value": "xywh=463,1577,969,413"}}}}}, 
+{"@context": "http://iiif.io/api/presentation/2/context.json", "@id": "dd687ad4-a845-4f66-8bcc-ad5a12d58c4e", "@type": "oa:Annotation", "motivation": "sc:painting", "annotatedBy": {"name": "ben"}, "resource": {"@type": "dctypes:Text", "format": "text/html", "chars": "<p>testing saving a user annotation</p>", "language": "en"}, "on": {"full": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/canvas/32862204.5243.emory.edu$0", "@type": "oa:SpecificResource", "within": {"@id": "https://0.0.0.0:3000/iiif/v2/readux:t9vft/manifest", "@type": "sc:Manifest"}, "selector": {"@type": "oa:FragmentSelector", "value": "xywh=970,917,1396,340", "item": {"@type": "oa:Choice", "value": "<svg xmlns=\'http://www.w3.org/2000/svg\'><path xmlns=\\"http://www.w3.org/2000/svg\\" d=\\"M970.30579,916.59504h698.00413v0h698.00413v170.21488v170.21488h-698.00413h-698.00413v-170.21488z\\" data-paper-data=\\"{&quot;strokeWidth&quot;:1,&quot;rotation&quot;:0,&quot;deleteIcon&quot;:null,&quot;rotationIcon&quot;:null,&quot;group&quot;:null,&quot;newlyCreatedStrokeFactor&quot;:5,&quot;newlyCreated&quot;:true,&quot;editable&quot;:true,&quot;annotation&quot;:null}\\" id=\\"rectangle_8500a0f5-ae4c-4ad2-af9b-1f090825cb15\\" fill-opacity=\\"0\\" fill=\\"#00bfff\\" fill-rule=\\"nonzero\\" stroke=\\"#00bfff\\" stroke-width=\\"5\\" stroke-linecap=\\"butt\\" stroke-linejoin=\\"miter\\" stroke-miterlimit=\\"10\\" stroke-dasharray=\\"\\" stroke-dashoffset=\\"0\\" font-family=\\"none\\" font-weight=\\"none\\" font-size=\\"none\\" text-anchor=\\"none\\" style=\\"mix-blend-mode: normal\\"/></svg>", "default": {"@type": "oa:FragmentSelector", "value": "xywh=970,917,1396,340"}}}}}]'
+JSON.parse(raw)
+exit
+User.all.each { |u| u.password='password'; u.password_confirmation='password'; u.save! }
+exit
+page = 
+work = Work.find 5
+work.ia_work
+IaWork.pluck(:ia_path)
+IaWork.pluck(:detail_url)
+work.pages.last
+work.pages.last.ia_leaf
+work.pages.last
+exit
+results = Collection.where("title_like '%?%'), search_string)
+'
+results = Collection.where("title_like '%?%'"), search_string)
+search_string="Woodson"
+results = Collection.where("title_like '%?%'"), search_string)
+results = Collection.where("title_like '%?%'", search_string)
+results = Collection.where("title like '%?%'", search_string)
+results = Collection.where("title like ?", search_string)
+results = Collection.where("title like ?", search_string).coun
+results = Collection.where("title like ?", search_string).count
+results = Collection.where("title like ? OR owner_id in (select id from users where owner=1 and display_name like ?)", search_string, search_string).count
+Collection.first
+results = Collection.where("title like ? OR owner_user_id in (select id from users where owner=1 and display_name like ?)", search_string, search_string).count
+User.last
+Work.last
+Work.last.owner
+search_string="BPOC"
+results = Collection.where("title like ? OR owner_user_id in (select id from users where owner=1 and display_name like ?)", search_string, search_string).count
+search_string="POC"
+results = Collection.where("title like ? OR owner_user_id in (select id from users where owner=1 and display_name like ?)", search_string, search_string).count
+search_string="%POC%"
+results = Collection.where("title like ? OR owner_user_id in (select id from users where owner=1 and display_name like ?)", search_string, search_string).count
+exit
+require 'json'
+require 'csv'
+Dir.glob "*"
+raw_audacity = File.read "PS3537E915A6x1974.txt"
+CSV.open "PS3537E915A6x1974.txt"
+CSV.open "PS3537E915A6x1974.txt", :col_sep => "\t"
+csv = CSV.open "PS3537E915A6x1974.txt", :col_sep => "\t"
+csv.to_a
+raw_point_anno = csv.to_a.first
+raw_range_anno = csv.to_a.last
+model
+model = <<EOF
+{
+  "@context": "http://iiif.io/api/presentation/3/context.json",
+  "id": "https://example.org/iiif/book1/annotation/p0001-image",
+  "type": "Annotation",
+  "motivation": "painting",
+  "body": {
+    "id": "https://example.org/images/page1.jpg",
+    "type": "Image"
+  },
+  "target": "https://example.org/iiif/book1/canvas/p1"
+}
+EOF
+JSON.parse(model)
+wa = JSON.parse(model)
+wa["motivation"]
+wa["motivation"]="commenting"
+wa["body"]
+wa["body"]["id"]=nil
+wa["body"]
+wa["body"].remove("id")
+body = { "type" => "TextualBody", "value" => raw_point_anno[3], "format" => "text/plain" }
+wa["body"] = target
+wa["body"] = body
+wa
+pp wa
+wa["target"] = "https://raw.githubusercontent.com/BrumfieldLabs/cookbook-recipes/master/recipe/0103-poetry-reading-annotations/manifest/canvas/segment1"
+pp wa;0
+wa["id"] = "https://raw.githubusercontent.com/BrumfieldLabs/cookbook-recipes/master/recipe/0103-poetry-reading-annotations/manifest/canvas/annotation1"
+pp wa;0
+selector = {"type" => "RangeSelector", "t" => raw_range_anno[1] }
+pp wa;0
+wa["target"] = { "source" => wa["target"], "selector" => selector }
+pp wa;0
+wa["@context"] = "http://www.w3.org/ns/anno.jsonld"
+pp wa;0
+body = { "type" => "TextualBody", "value" => raw_point_anno[2], "format" => "text/plain" }
+wa["body"] = body
+pp wa;0
+wa.to_json
+print wa.to_json
+wa_bak = wa.dup
+wa_bak = wa.deep_copy
+wa
+wa["id"] = "https://raw.githubusercontent.com/BrumfieldLabs/cookbook-recipes/master/recipe/0103-poetry-reading-annotations/manifest/canvas/annotation2"
+wa_bak
+raw_range_anno
+body = { "type" => "TextualBody", "value" => raw_range_anno[2], "format" => "text/plain" }
+wa["body"] = body
+wa
+pp wa;0
+READLINES.history
+Readline.HISTORY
+Readline.history
+Readline.class
+Readline.class.HISTORY
+Readline.class.history
+print Readline::HISTORY.to_a.join("\n")
