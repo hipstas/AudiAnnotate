3ac58b077e44204b654a66b3ef82333b5b55c5ce
Ben W. Brumfield <benwbrum@gmail.com>
2019-10-29 12:46:42 -0500

    Stub routes and templates

--- AudiAnnotateWeb/config/routes.rb
+++ AudiAnnotateWeb/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'project#all'
+
   get 'project/all'
   get 'user/login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
