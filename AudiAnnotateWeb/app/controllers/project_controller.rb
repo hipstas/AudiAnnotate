d5eb88283d8ad355aadba24d8b20bbf57feca324
Ben W. Brumfield <benwbrum@gmail.com>
2019-10-29 14:16:49 -0500

    Stub Octokit integration

--- AudiAnnotateWeb/app/controllers/project_controller.rb
+++ AudiAnnotateWeb/app/controllers/project_controller.rb
@@ -1,4 +1,19 @@
 class ProjectController < ApplicationController
   def all
+
+  	# TODO change this to use the search API for all public AA repos
+  	@repos = Octokit.repositories('saracarl')
+
+  end
+
+  def mine
+    # TODO  replace hard-wired string with current user
+    @repos = Octokit.repositories('saracarl')
+  end
+
+  def show
+    user_name = params[:user_name]
+    repo_name = params[:repo_name]
+    @repo = Octokit.repository("#{user_name}/#{repo_name}")
   end
 end
