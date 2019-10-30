61ae4545c94b7b217fa55d6ee4d9290a2239639c
saracarl <saracarl@gmail.com>
2019-10-30 08:44:35 -0500

    background image, github auth

--- AudiAnnotateWeb/app/javascript/packs/application.js
+++ AudiAnnotateWeb/app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
-
+require("bootstrap/dist/js/bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
