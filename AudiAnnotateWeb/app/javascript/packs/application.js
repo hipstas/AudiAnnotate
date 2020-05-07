bf704168f386e10c627255d7bdca51b8b3d94424
Ben W. Brumfield <benwbrum@gmail.com>
2020-05-07 14:46:44 -0500

    Added datatables to web app display of annotations

--- AudiAnnotateWeb/app/javascript/packs/application.js
+++ AudiAnnotateWeb/app/javascript/packs/application.js
@@ -7,6 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
+require("datatables.net")
+require('datatables.net-bs4')
+require("datatables.net-bs4/css/dataTables.bootstrap4.min.css")
+
+import $ from 'jquery'; 
+global.$ = jQuery;
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
