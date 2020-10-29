e08706b44b5bd925e8608041c4d36919b6ece6b9
Ben W. Brumfield <benwbrum@gmail.com>
2020-10-29 11:35:27 -0500

    Support Premiere Pro for #122

--- AudiAnnotateWeb/app/models/annotation_file.rb
+++ AudiAnnotateWeb/app/models/annotation_file.rb
@@ -1,23 +1,6 @@
 class AnnotationFile
   attr_accessor :layer
 
-
-
-PROTOTYPE = <<EOF
-{
-  "@context": "http://iiif.io/api/presentation/3/context.json",
-  "id": "https://example.org/iiif/book1/annotation/p0001-image",
-  "type": "Annotation",
-  "motivation": "painting",
-  "body": {
-    "id": "https://example.org/images/page1.jpg",
-    "type": "Image"
-  },
-  "target": "https://example.org/iiif/book1/canvas/p1"
-}
-EOF
-
-
   def initialize(canvas, layer, uploaded_file)
     @canvas = canvas
     @uploaded_file = uploaded_file
@@ -30,8 +13,9 @@ EOF
     config = {}
     begin
       csv = CSV.read(@uploaded_file, col_sep: "\t", quote_char: "𒊬") # people may use double-quotes but should not be annotating in cuneiform
+      # configuration specific to Audacity uploads
       config = {
-        col_sep: ",",
+        col_sep: "\t",
         layer_col: nil,
         text_col: 2,
         start_col: 0,
@@ -42,6 +26,8 @@ EOF
 
       contents = File.read(@uploaded_file)
       detection = CharlockHolmes::EncodingDetector.detect(contents)
+
+      # configuration specific to Adobe Premiere
       config = {
         col_sep: "\t",
         layer_col: 0,
@@ -55,22 +41,41 @@ EOF
       # csv.map { |r| r.count }.max == csv.map { |r| r.count }.in
       # to see whether the results of csv parsing are rectangular or jagged
 
-      # TODO map layer label column, aggregate into a loop and write
-      # multiple files
-
-      # TODO differentiate between slug and label
 
       # TODO store config in first-class object project wide (to be filled in
       # by an import wizard)
-      csv = CSV.read("../../scratch/Camille 1921 annotations.csv", 
+      csv = CSV.read(@uploaded_file, 
                       :encoding => "bom|#{detection[:encoding]}",
-                      :col_sep => "\t", 
+                      :col_sep => config[:col_sep], 
                       :quote_char => '"', 
                       :liberal_parsing => true)
     end
 
-    Dir.mkdir(@canvas.canvas_path) unless Dir.exists?(@canvas.canvas_path)
-    File.write(annotation_page_file_path, page_contents(csv, config))
+
+    layers = {}
+    if config[:layer_col].nil?
+      layers[layer] = csv
+    else
+      if config[:headers]
+        start_row = 1
+      else
+        start_row = 0
+      end
+      labels = csv[start_row..].map{|row| row[config[:layer_col]]}.sort.uniq
+      labels.each do |label|
+        # find each subset of rows
+        target_rows = csv.dup.keep_if{|row| row[config[:layer_col]] == label}
+        # create a layer for each label and subset of rows
+        layers[label] = target_rows
+      end
+
+    end
+
+    layers.each_pair do |layer_label, rows|
+      page = AnnotationPage.from_csv(rows, config, layer_label, @canvas)
+      page.create
+    end
+
 
     @canvas.item.save(access_token)
   end
@@ -78,89 +83,6 @@ EOF
   def destroy(access_token)
     File.unlink(annotation_page_file_path)
     @canvas.item.save(access_token)
-  end
-
-  def seconds_from_raw(raw)
-    if md=raw.match(/(\d\d);(\d\d);(\d\d);(\d\d)/)
-      #this is adobe premiere export format
-      seconds=0.0
-      seconds += md[1].to_i*60*60 #take hours convert to seconds
-      seconds += md[2].to_i*60 #take minutes convert to seconds
-      seconds += md[3].to_i #add seconds
-      seconds += md[4].to_f/100 #add hundreths of seconds
-      seconds.to_s
-    else
-      raw
-    end
-  end
-
-  def page_contents(csv, config)
-    page = {
-      "@context": "http://iiif.io/api/presentation/3/context.json",
-      "id": "#{annotation_page_uri}",
-      "type": "AnnotationPage",
-      "label": @layer
-    }
-
-    items = []
-    csv.each_with_index do |row, i|
-      unless config[:headers] && i==0
-        wa = JSON.parse(PROTOTYPE)
-        # set the constants
-        wa["@context"] = "http://www.w3.org/ns/anno.jsonld"
-        wa["motivation"]=["supplementing", "commenting"]
-        body = { "type" => "TextualBody", "value" => row[config[:text_col]], "format" => "text/plain" }
-        wa["body"] = body
-        # TODO if on point vs range
-        # pull row[0] and row[1] into variables
-        row0=seconds_from_raw(row[config[:start_col]])
-        row1=seconds_from_raw(row[config[:end_col]])
-        # parse them into seconds
-        if row0==row1
-          # point selection
-          selector = { "type" => "PointSelector", "t" => row0 }
-        else
-          # range selection
-          selector = {"type" => "RangeSelector", "t" => "#{row0},#{row1}" }
-        end
-        wa["id"] = annotation_uri(i)
-        wa["target"] = { "source" => @canvas.canvas_id, "selector" => selector }
-        items << wa
-      end
-    end
-
-    page['items'] = items
-
-    JSON.pretty_generate(page)
-  end
-
-
-
-  #######################
-  # Manifest helpers
-  #######################
-  def slug
-    layer.gsub(/\W/, '-')
-  end
-
-  def annotation_page_file_path
-    File.join(@canvas.canvas_path, annotation_page_file)
-  end
-
-  def annotation_page_file
-    "#{slug}.json"
-  end
-
-  # def uri_root
-  #   "#{@item.uri_root}/#{slug}"
-  # end
-
-  def annotation_page_uri
-    "#{@canvas.canvas_id}/#{slug}.json"
-  end
-
-  def annotation_uri(index)
-    "#{@canvas.canvas_id}/#{slug}-annotation-#{index}.json"
   end
 
 
--- AudiAnnotateWeb/app/models/annotation_page.rb
+++ AudiAnnotateWeb/app/models/annotation_page.rb
@@ -1,12 +1,36 @@
 class AnnotationPage
 
-  def initialize(canvas, page_path)
+PROTOTYPE = <<EOF
+{
+  "@context": "http://iiif.io/api/presentation/3/context.json",
+  "id": "https://example.org/iiif/book1/annotation/p0001-image",
+  "type": "Annotation",
+  "motivation": "painting",
+  "body": {
+    "id": "https://example.org/images/page1.jpg",
+    "type": "Image"
+  },
+  "target": "https://example.org/iiif/book1/canvas/p1"
+}
+EOF
+
+
+  #############################################
+  # Methods to READ an annotation page
+  #############################################
+  def initialize(canvas, page_path=nil)
     @canvas = canvas
-    @page = JSON.load(File.open(page_path))
+    if page_path
+      @page = JSON.load(File.open(page_path))
+    end
   end
 
   def label
-    @page['label']
+    @label || @page['label']
+  end
+
+  def label=(label)
+    @label = label
   end
 
   def annotations
@@ -16,5 +40,122 @@ class AnnotationPage
   def annotation_page_uri
     @page['id']
   end
+
+  def rows=(rows)
+    @rows=rows
+  end
+
+  def config=(config)
+    @config=config
+  end
+
+  #############################################
+  # Methods to WRITE an annotation page
+  #############################################
+  def self.from_csv(rows, config, label, canvas)
+    Dir.mkdir(canvas.canvas_path) unless Dir.exists?(canvas.canvas_path)
+
+    page = AnnotationPage.new(canvas)
+    page.label=label
+    page.rows=rows
+    page.config=config
+
+    page
+  end
+
+
+  def create
+    Dir.mkdir(@canvas.canvas_path) unless Dir.exists?(@canvas.canvas_path)
+
+    File.write(annotation_page_file_path, page_contents(@rows, @config))
+  end
+
+  def self.label_to_slug(label)
+
+  end
+
+
+  def seconds_from_raw(raw)
+    if md=raw.match(/(\d\d);(\d\d);(\d\d);(\d\d)/)
+      #this is adobe premiere export format
+      seconds=0.0
+      seconds += md[1].to_i*60*60 #take hours convert to seconds
+      seconds += md[2].to_i*60 #take minutes convert to seconds
+      seconds += md[3].to_i #add seconds
+      seconds += md[4].to_f/100 #add hundreths of seconds
+      seconds.to_s
+    else
+      raw
+    end
+  end
+
+  def page_contents(csv, config)
+    page = {
+      "@context": "http://iiif.io/api/presentation/3/context.json",
+      "id": "#{annotation_page_uri}",
+      "type": "AnnotationPage",
+      "label": @label
+    }
+
+    items = []
+    csv.each_with_index do |row, i|
+      wa = JSON.parse(PROTOTYPE)
+      # set the constants
+      wa["@context"] = "http://www.w3.org/ns/anno.jsonld"
+      wa["motivation"]=["supplementing", "commenting"]
+      body = { "type" => "TextualBody", "value" => row[config[:text_col]], "format" => "text/plain" }
+      wa["body"] = body
+      # TODO if on point vs range
+      # pull row[0] and row[1] into variables
+      start_seconds=seconds_from_raw(row[config[:start_col]])
+      end_seconds=seconds_from_raw(row[config[:end_col]])
+      # parse them into seconds
+      if start_seconds==end_seconds
+        # point selection
+        selector = { "type" => "PointSelector", "t" => start_seconds }
+      else
+        # range selection
+        selector = {"type" => "RangeSelector", "t" => "#{start_seconds},#{end_seconds}" }
+      end
+      wa["id"] = annotation_uri(i)
+      wa["target"] = { "source" => @canvas.canvas_id, "selector" => selector }
+      items << wa
+    end
+
+    page['items'] = items
+
+    JSON.pretty_generate(page)
+  end
+
+
+
+  #######################
+  # Manifest helpers
+  #######################
+  def slug
+    label.gsub(/\W/, '-').downcase
+  end
+
+  def annotation_page_file_path
+    File.join(@canvas.canvas_path, annotation_page_file)
+  end
+
+  def annotation_page_file
+    "#{slug}.json"
+  end
+
+  # def uri_root
+  #   "#{@item.uri_root}/#{slug}"
+  # end
+
+  def annotation_page_uri
+    "#{@canvas.canvas_id}/#{slug}.json"
+  end
+
+  def annotation_uri(index)
+    "#{@canvas.canvas_id}/#{slug}-annotation-#{index}.json"
+  end
+
+
 
 end
